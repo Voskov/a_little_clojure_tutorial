8fb735b92b9ae769dc0d9dae8d20a9e467973296
Ariel Voskov <avoskov@nexxen.com>
2025-03-20 11:28:36 +0200

    some more maps

--- src/a_little_clojure_training/chapter6/maps.clj
+++ src/a_little_clojure_training/chapter6/maps.clj
@@ -86,3 +86,35 @@ me
 
 (map :one [{:one 1} {:one "one"}])
 
+(def bob {:bob {:first "Bobby" :last "Martin"}})
+(def bill {:bill {:first "Billy" :last "Smith"}})
+
+bob
+bill
+(merge bob bill)
+(def people (merge bob bill))
+people
+
+(:bob people)
+(people :bob)
+(= (:bob people) (people :bob))
+
+(get-in people [:bob :first])
+(get-in people [:bob])
+(assoc-in people [:bob :first] "Robert")
+
+(def people2 (-> people
+                 (assoc-in [:bob :age] 72)
+                 (assoc-in [:bill :age] 61)))
+
+(prn people2)
+(update-in people2 [:bob :age] inc)
+
+(def people2 (-> people
+                 (assoc-in [:bob :first] "Robert")
+                 (assoc-in [:bill :first] "William")))
+(def people3 (-> people2
+                 (assoc-in [:bob :age] 72)
+                 (assoc-in [:bill :age] 61)))
+(update-in people3 [:bob :age] inc)
+(reduce #(update-in %1 [(key %2) :age] inc) people3 people3)
